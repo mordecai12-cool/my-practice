de7c4d713a65db2958d84d9c164956fd509eeb53
omordeca <marvellousmordecaiotiti@gmail.com>
2026-03-19 15:56:02 +0000

    my-practice

---
+++ AOrAn.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func fixAOrAn(word string) string {
+	word = strings.ReplaceAll(word, "A", "An")
+	word = strings.ReplaceAll(word, "An book", "A book")
+	return word
+}
+func main() {
+	fmt.Println(fixAOrAn("there it was. A amazing rock. A honest man. A book"))
+}
