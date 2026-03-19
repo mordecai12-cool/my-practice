a20eb0fd433b1497b435cd4371241a73ae00554a
omordeca <marvellousmordecaiotiti@gmail.com>
2026-03-19 14:03:32 +0000

    my-practice

---
+++ capFirstLetter.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func capFirstLetter(words string) string {
+	return strings.ToUpper(words[:1]) + strings.ToLower(words[1:])
+}
+func main() {
+	fmt.Println(capFirstLetter("hELLO"))
+	fmt.Println(capFirstLetter("WORLD"))
+}
