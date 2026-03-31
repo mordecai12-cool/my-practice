2cd48f63b0b5b080f6eac9edeffbb3954290ef57
omordeca <marvellousmordecaiotiti@gmail.com>
2026-03-31 10:12:20 +0100

    my-practice

---
+++ Assig.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+	func main() {
+		var Name = "marvy"
+		var Age = 19
+		var City = "Canada"
+		var Email = "marvy@gmail.com"
+
+		fmt.Println("Name :", Name)
+		fmt.Println("Age :", Age)
+		fmt.Println("City :", City)
+		fmt.Println("Email :", Email)
+	}
+*/
+/*func main() {
+	var name string = "sunshine"
+	var price float64 = 3.457
+	var quality int = 70
+	var stock bool = true
+
+	fmt.Println("name :", name)
+	fmt.Println("price :", price)
+	fmt.Println("quality :", quality)
+	fmt.Println("stock :", stock)
+}
+*/
+func main() {
+	
+}
