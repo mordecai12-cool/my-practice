064d74990f2b1206fb413ef6df553cdb376085c8
mordecai12-cool <marvellousmordecaiotiti@gmail.com>
2026-07-16 13:57:58 +0100

    updated

--- python/main.py
+++ python/main.py
@@ -1,8 +1,14 @@
 x = "awesome"
 
+# def myfunc():
+#     print("python is " + x)
+
+# myfunc()   
+
+#If you use the global keyword, the variable belongs to the global scope:
 def myfunc():
+    global x
     x = "fantastic"
-    print("python is " + x )
 
 myfunc()
 print("python is " + x)    
