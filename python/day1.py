187cba42ac6d36d2e6f09152de205d0ef6f84eb6
mordecai12-cool <marvellousmordecaiotiti@gmail.com>
2026-09-07 10:38:24 +0100

    w3school

---
+++ python/day1.py
@@ -0,0 +1,15 @@
+#Boolean Values
+
+# a = 200
+# b = 330
+
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than a")
+
+#When you compare two values, the expression is evaluated and Python returns the Boolean answer:
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
