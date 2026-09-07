9523f75ce8c60cd4c85b765997ba3a8359435f93
mordecai12-cool <marvellousmordecaiotiti@gmail.com>
2026-09-07 11:14:17 +0100

    Boolean Values

--- python/day1.py
+++ python/day1.py
@@ -13,3 +13,23 @@ print(10 > 9)
 print(10 == 9)
 print(10 < 9)
 
+#Evaluate a string and a number:
+class myclass():
+    def __len__(self):
+        return 0
+
+myobj = myclass()
+print(bool(myobj)) # False
+
+#Print "YES!" if the function returns True, otherwise print "NO!":
+def myFunction():
+    return True
+
+if myFunction():
+    print("YES!")
+else:
+    print("NO!")
+
+#Check if an object is an integer or not:
+x = 200
+print(isinstance(x, int))
